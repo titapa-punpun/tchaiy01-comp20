4b287295090fba820ec10ccd2fb2c11e6539bbc0
titapapunne <titapa.chaiyakiturajai@tufts.edu>
2018-11-01 12:26:15 -0400

    package.json added

--- lab8/index.js
+++ lab8/index.js
@@ -13,3 +13,11 @@ app.get("/scores.json", function(request, response) {
 app.get("/", function(request, response) {
 	response.send("Go away!");
 });
+
+// need to install express
+// package = software 
+// npm = package manager (thousands of packages you can use)
+// run with node index.js 
+// don't want to keep running "npm install express"
+// if you want to use many dependencies in your work, use package.json (like
+// you recipe card--grocery shopping) 
